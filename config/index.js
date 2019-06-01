1ab410c26950a3da9bfdb1f4ff9dea1e82b10275
klequis <klequis@gmail.com>
2019-05-31 20:14:32 -0700

    jkl

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 /* istanbul ignore file */
 const mongoUrl = env => {
   if (env === 'test') {
-    // good: return 'mongodb://localhost:27017'
-    // mongodb://superadmin:thepianohasbeendrinking@<host>:<port>
+    // return 'mongodb://localhost:27017'
+    // example: mongodb://superadmin:thepianohasbeendrinking@<host>:<port>
     return 'mongodb://localadmin:a9vChJeSqk&uUG@localhost:27017'
   } else {
     return 'mongodb+srv://todo-db-admin:D92dARWONO0t16uF@todo-cluster0-ilc7v.mongodb.net/test?retryWrites=true'
